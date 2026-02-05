06c64c893a1b977de0e4eab68ab89938822f05d3
GonzaloSantillan <lsantillangonzalo73@gmail.com>
2026-02-05 04:53:35 -0300

    añadi verificacion de longitud de tarea para que no se pueda agregar tareas de menos de 3 caracteres

--- src/components/InputTareas.jsx
+++ src/components/InputTareas.jsx
@@ -1,32 +1,43 @@
-import React,{useState} from "react";
-import "../styles/InputTareas.css"
+import React, { useState } from "react";
+import "../styles/InputTareas.css";
 
-function InputTareas({agregarTarea}){
+function InputTareas({ agregarTarea }) {
+  const [input, setInput] = useState("");
+  const [error, setError] = useState(0)
 
-  const [input,setInput] = useState("")
+  const manejarCambio=(e) => {
+    setInput(e.target.value);
+    setError(e.target.value.length)
+  };
 
-  const manejarCambio=(e)=>{
-    setInput(e.target.value)
-  }
+  const manejarEnvio=(e) => {
+    e.preventDefault();
+    const tareaNueva = {
+      texto: input,
+      id: crypto.randomUUID(),
+      completado: false,
+    };
+    agregarTarea(tareaNueva);
+  };
 
-  const manejarEnvio=(e)=>{
-    e.preventDefault()
-    const tareaNueva={
-      texto:input,
-      id:crypto.randomUUID(),
-      completado:false
-    }
-    agregarTarea(tareaNueva)
-  }
-
-  return(
+  return (
     <>
       <form className="formulario" onSubmit={manejarEnvio}>
-        <input className="input-tarea" type="text" placeholder="Escriba la tarea..." name="tarea" onChange={manejarCambio} />
-        <button className="agregar-tarea" type="submit">Agregar</button>
+        <input
+          className="input-tarea"
+          type="text"
+          placeholder="Escriba la tarea..."
+          name="tarea"
+          onChange={(e)=>{manejarCambio(e)}}
+          autoComplete="off"
+        />
+        <button className="agregar-tarea" type="submit">
+          Agregar
+        </button>
       </form>
+      <p className="mensaje-error">{error<3?`Ingresa al menos 3 caracteres (llevas ${error}).`:``}</p>
     </>
-  )
+  );
 }
 
-export default InputTareas
+export default InputTareas;
